ea40624405cae8d26ae0bd1eebc5343bbf347ecb
David Kemp <davidbkemp@gmail.com>
2013-01-12 10:32:52 +1100

    Simplified by using

--- src/repeatedController.js
+++ src/repeatedController.js
@@ -23,38 +23,24 @@ var MainController = function ($scope) {
 var ItemController = function ($scope) {
     $scope.item = $scope.items[$scope.$index];
 
-    // NOTE: Cannot rely on $index as items can be deleted and inserted into the middle of the array.
-
-    var itemIndex = function () {
-        var index;
-        for (index = 0; index < $scope.items.length; index++) {
-            if ($scope.items[index] === $scope.item) {
-                return index;
-            }
-        };
-        throw "Can't find the item??";
-    };
 
     $scope.deleteItem = function () {
-        $scope.items.splice(itemIndex(), 1);
+        $scope.items.splice($scope.$index, 1);
     };
 
     $scope.moveUp = function () {
-        var currentIndex = itemIndex();
-        if (currentIndex == 0) return;
-        $scope.items.splice(currentIndex, 1);
-        $scope.items.splice(currentIndex - 1, 0, this.item);
+        if ($scope.$index == 0) return;
+        $scope.items.splice($scope.$index, 1);
+        $scope.items.splice($scope.$index - 1, 0, this.item);
     };
 
     $scope.moveDown = function () {
-        var currentIndex = itemIndex();
-        if (currentIndex == $scope.items.length) return;
-        console.log('moving down')
-        $scope.items.splice(currentIndex, 1);
-        $scope.items.splice(currentIndex + 1, 0, this.item);
+        if ($scope.$index == $scope.items.length) return;
+        $scope.items.splice($scope.$index, 1);
+        $scope.items.splice($scope.$index + 1, 0, this.item);
     };
 
     $scope.showFoo = function () {
-        alert("Foo is: " + ($scope.foo ? "checked" : "not checked") + ". Note foo is kept directly on the controller.");
+        alert("Foo is: " + ($scope.foo ? "checked" : "not checked") + ". Note foo is kept directly on the controller.  $index is " + $scope.$index);
     }
 }
