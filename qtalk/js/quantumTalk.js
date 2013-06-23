c1a7404446015e21506960270ff54b067cd38abd
David Kemp <davidbkemp@gmail.com>
2013-06-23 11:37:43 +1000

    A little refactoring

--- qtalk/js/quantumTalk.js
+++ qtalk/js/quantumTalk.js
@@ -1,3 +1,4 @@
+
 jQuery(function ($) {
     function asBinary(i, length) {
         var paddedString = "00000000000000000000000000000000" + i.toString(2);
@@ -12,30 +13,30 @@ jQuery(function ($) {
     var textHeight =  qStateCalibrationElement.height();
     var textWidth =  qStateCalibrationElement.width();
 
-    function renderQState(selector, dataset) {
+    function renderQState(svgSelector, data) {
 
-        var width = 500;
-        var height = 400;
-        var bitCount = dataset.length;
-        var diameter = height / bitCount;
-        var radius = diameter / 2;
+        var svgWidth = 500;
+        var svgHeight = 400;
+        var bitCount = data.length;
+        var maxDiameter = svgHeight / bitCount;
+        var maxRadius = maxDiameter / 2;
 
-        $(selector).data('bitCount', bitCount);
-        $(selector).data('radius', radius);
+        $(svgSelector).data('bitCount', bitCount);
+        $(svgSelector).data('maxRadius', maxRadius);
 
-        var svg = d3.select(selector);
+        var svg = d3.select(svgSelector);
 
-        svg.attr('width', width).attr('height', height);
+        svg.attr('width', svgWidth).attr('height', svgHeight);
 
         var states = svg.selectAll('.qstate')
-            .data(dataset)
+            .data(data)
             .enter()
             .append('text')
             .attr('class', 'qstate');
 
         states.attr('x', 0)
             .attr('y', function(d, i) {
-                return i * diameter + radius + (textHeight / 3);
+                return i * maxDiameter + maxRadius + (textHeight / 3);
             })
             .attr('opacity', computeOpacity)
             .text(function(d, i) {
@@ -43,83 +44,62 @@ jQuery(function ($) {
             });
 
         var amplitudes = svg.selectAll('.amplitude')
-            .data(dataset)
+            .data(data)
             .enter()
             .append("circle")
             .attr('class', 'amplitude');
 
-        amplitudes.attr('cx', radius + 1.3 * textWidth)
+        amplitudes.attr('cx', maxRadius + 1.3 * textWidth)
             .attr('cy', function(d, i) {
-                return radius + i * diameter;
+                return maxRadius + i * maxDiameter;
             })
             .attr("r", function(d) {
-                return  d * radius;
+                return  d * maxRadius;
             })
             .attr('fill', '#add8e6');
 
 
     }
 
+    function transitionQState(svgSelector, dataSet) {
+        var maxRadius = $(svgSelector).data('maxRadius');
+
+        var delay = 2000;
+
+        var svg = d3.select(svgSelector);
+
+        svg.selectAll('.amplitude')
+            .data(dataSet)
+            .transition()
+            .duration(delay)
+            .attr("r", function(d) {
+                return  d * maxRadius;
+            });
+
+        svg.selectAll('.qstate')
+            .data(dataSet)
+            .transition()
+            .duration(delay)
+            .attr('opacity', computeOpacity);
+    }
+
     renderQState("#classical", [ 0, 0, 0, 0, 0, 0, 1, 0 ]);
     renderQState("#quantum", [ 0.75, 0.75, 0, 0, 0.75, 0, 0.75, 0 ]);
 
     renderQState("#peek", [ 0.75, 0.75, 0, 0, 0.75, 0, 0.75, 0 ]);
-
     $('#peekButton').click(function () {
         $('#peekingEye').show();
-
-        var radius = $('#peek').data('radius');
-
-        var data = [ 0, 0, 0, 0, 0, 0, 1, 0 ];
-
-        var delay = 2000;
-
-        var svg = d3.select("#peek");
-
-        svg.selectAll('.amplitude')
-            .data(data)
-            .transition()
-            .duration(delay)
-            .attr("r", function(d) {
-                return  d * radius;
-            });
-
-        svg.selectAll('.qstate')
-            .data(data)
-            .transition()
-            .duration(delay)
-            .attr('opacity', computeOpacity);
+        transitionQState('#peek', [ 0, 0, 0, 0, 0, 0, 1, 0 ]);
     });
 
 
     renderQState("#amplitudes", [ 0, 0.3, 0.75, 0, 0.3, 0, 0.4, 0 ]);
 
     renderQState("#favouringLargerAmplitudes", [ 0, 0.3, 0.75, 0, 0.3, 0, 0.4, 0 ]);
-
     $('#favouringLargerAmplitudesPeekButton').click(function () {
         $('#favouringLargerAmplitudesPeekingEye').show();
-
-        var radius = $('#favouringLargerAmplitudes').data('radius');
-
-        var data = [ 0, 0, 1, 0, 0, 0, 0, 0 ];
-
-        var delay = 2000;
-
-        var svg = d3.select("#favouringLargerAmplitudes");
-
-        svg.selectAll('.amplitude')
-            .data(data)
-            .transition()
-            .duration(delay)
-            .attr("r", function(d) {
-                return  d * radius;
-            });
-
-        svg.selectAll('.qstate')
-            .data(data)
-            .transition()
-            .duration(delay)
-            .attr('opacity', computeOpacity);
+        transitionQState('#favouringLargerAmplitudes', [ 0, 0, 1, 0, 0, 0, 0, 0 ]);
     });
+    
 });
 
