73a294601efdfff1ad933b8c0fa6cb02af17f448
David Kemp <davidbkemp@gmail.com>
2013-06-25 21:03:55 +1000

    Use group element trasform to rotate, translate, and scale the amplitudes

--- qtalk/js/quantumTalk.js
+++ qtalk/js/quantumTalk.js
@@ -16,9 +16,8 @@ jQuery(function ($) {
         });
     }
 
-    var qStateCalibrationElement = $('#calibration .qstate');
-    var textHeight =  qStateCalibrationElement.height();
-    var textWidth =  qStateCalibrationElement.width();
+    var textHeight =  42;
+    var textWidth =  58;
 
     function renderStateLabels(svgSelector, dataSet) {
         var maxRadius = $(svgSelector).data('maxRadius');
@@ -47,79 +46,58 @@ jQuery(function ($) {
         };
     }
 
-    function renderAmplitudeCircles(svgSelector, dataSet) {
-        var maxRadius = $(svgSelector).data('maxRadius');
-
-        d3.select(svgSelector).selectAll('.amplitude')
-            .data(dataSet)
-            .enter()
-            .append("circle")
-            .attr('class', 'amplitude')
-            .attr('cx', function(d, i) {
-                return amplitudeCircle(maxRadius, d, i).cx;
-            })
-            .attr('cy',  function(d, i) {
-                return amplitudeCircle(maxRadius, d, i).cy;
-            })
-            .attr("r",  function(d, i) {
-                return amplitudeCircle(maxRadius, d, i).radius;
-            });
-    }
-
-    function renderPhases(svgSelector, dataSet) {
-        var maxRadius = $(svgSelector).data('maxRadius');
-
-        var phaseLineGroups = d3.select(svgSelector).selectAll('.phaseLineGroup')
-            .data(dataSet)
-            .enter()
-            .append("g")
-            .attr('class', 'phaseLineGroup')
-            .attr('transform', function(d, i) {
-                var circleData = amplitudeCircle(maxRadius, d, i);
-                var x = circleData.cx;
-                var y = circleData.cy;
-                var degrees = 180 * d.phase / Math.PI;
-                return 'rotate(' + degrees + ',' + x + ',' + y + ')';
-            });
-
-        phaseLineGroups
-            .append("line")
-            .attr('class', 'phaseLine')
-            .attr('x1', function(d, i) {
-                return amplitudeCircle(maxRadius, d, i).cx;
-            })
-            .attr('y1', function(d, i) {
-                return amplitudeCircle(maxRadius, d, i).cy;
-            })
-            .attr('x2', function(d, i) {
-                var circleData = amplitudeCircle(maxRadius, d, i);
-                return circleData.cx + circleData.radius;
-            })
-            .attr('y2', function(d, i) {
-                return amplitudeCircle(maxRadius, d, i).cy;
-            });
-
-        phaseLineGroups
-            .append("polygon")
-            .attr('class', 'phaseLineEnd')
-            .attr('points', function(d, i) {
-                var headLength = amplitudeCircle(maxRadius, d, i).radius/6;
-                console.log("head length: " + headLength)
-                var circleData = amplitudeCircle(maxRadius, d, i);
-                var x1 = circleData.cx + circleData.radius - headLength;
-                var y1 = amplitudeCircle(maxRadius, d, i).cy - headLength/2;
-                var x2 = x1;
-                var y2 = y1 + headLength;
-                var x3 = circleData.cx + circleData.radius;
-                var y3 = amplitudeCircle(maxRadius, d, i).cy;
-                return x1 + "," + y1 + " " + x2 + "," + y2 + " " +  x3 + "," + y3
-            });
-
+    function createTransformString(maxRadius, dataElement, index) {
+        var circleData = amplitudeCircle(maxRadius, dataElement, index);
+        var x = circleData.cx;
+        var y = circleData.cy;
+        var degrees = 180 * dataElement.phase / Math.PI;
+        var scale = dataElement.magnitude;
+        return 'translate(' + x + ',' + y + ') scale(' + scale + ') rotate(' + degrees + ')';
     }
 
     function renderAmplitudes(svgSelector, dataSet, options) {
-        renderAmplitudeCircles(svgSelector, dataSet);
-        if (options.showPhases) renderPhases(svgSelector, dataSet);
+
+        var maxRadius = $(svgSelector).data('maxRadius');
+
+        var amplitudeGroup = d3.select(svgSelector).selectAll('.amplitude')
+            .data(dataSet)
+            .enter()
+            .append('g')
+            .attr('class', 'amplitude')
+            .attr('transform', function(d, i) {
+                return createTransformString(maxRadius, d, i);
+            });
+
+        amplitudeGroup
+            .append("circle")
+            .attr('class', 'amplitudeCircle')
+            .attr('cx', 0)
+            .attr('cy', 0)
+            .attr("r",  maxRadius);
+
+        if (options.showPhases) {
+            amplitudeGroup
+                .append("line")
+                .attr('class', 'phaseLine')
+                .attr('x1', 0)
+                .attr('y1', 0)
+                .attr('x2', maxRadius)
+                .attr('y2', 0);
+
+            amplitudeGroup
+                .append("polygon")
+                .attr('class', 'phaseLineEnd')
+                .attr('points', function(d, i) {
+                    var headLength = maxRadius/4;
+                    var x1 = maxRadius - headLength;
+                    var y1 = -headLength/2;
+                    var x2 = x1;
+                    var y2 = y1 + headLength;
+                    var x3 = maxRadius;
+                    var y3 = 0;
+                    return x1 + "," + y1 + " " + x2 + "," + y2 + " " +  x3 + "," + y3
+                });
+        }
     }
 
     function renderQState(svgSelector, dataSet, options) {
@@ -148,19 +126,20 @@ jQuery(function ($) {
 
         var svg = d3.select(svgSelector);
 
-        svg.selectAll('.amplitude')
-            .data(dataSet)
-            .transition()
-            .duration(delay)
-            .attr("r", function(d, i) {
-                return amplitudeCircle(maxRadius, d, i).radius;
-            });
-
         svg.selectAll('.qstate')
             .data(dataSet)
             .transition()
             .duration(delay)
             .attr('opacity', computeOpacity);
+
+        svg.selectAll('.amplitude')
+            .data(dataSet)
+            .transition()
+            .duration(delay)
+            .attr('transform', function(d, i) {
+                return createTransformString(maxRadius, d, i);
+            });
+
     }
 
     renderQState("#classical", [ 0, 0, 0, 0, 0, 0, 1, 0 ]);
