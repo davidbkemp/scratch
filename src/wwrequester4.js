7b6f60a4d532331ede8f5336bbaaba7d8d0799f7
David Kemp <davidgithub@lizanddavid.com>
2013-08-24 19:49:57 +1000

    Update and rename wwrequester3.js to wwrequester4.js

--- src/wwrequester4.js
+++ src/wwrequester4.js
@@ -3,13 +3,17 @@
 function handleClick() {
     alert($('#input').val());
 
-    var worker = new Worker('wwworker.js');
+    var worker = new Worker('src/wwworker.js');
 
+    alert('created new worker')
     worker.addEventListener('message', function(e) {
         alert('Worker said: ' + e.data);
      }, false);
 
+     alert('added listener')
+
      worker.postMessage($('#input').val());
+     alert('posted message')
 }
 
 $(function(){
