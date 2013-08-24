9236c9d316c6a1a3b956793d7d64ca8dbfff3a61
David Kemp <davidgithub@lizanddavid.com>
2013-08-24 19:16:03 +1000

    Update wwrequester.js

--- src/wwrequester.js
+++ src/wwrequester.js
@@ -1,11 +1,10 @@
 (function($) {
 
 function handleClick() {
-    alert('clicked');
     alert($('#input').value);
 }
 
 $(function(){
-    $('#input').click(handleClick);
+    $('button').click(handleClick);
 });
 })(jQuery);
