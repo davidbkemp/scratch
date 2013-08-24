d580feee15541ff5bec487254401c0d2d92f9b14
David Kemp <davidgithub@lizanddavid.com>
2013-08-24 20:12:29 +1000

    Update and rename wwworker5.js to wwworker6.js

--- src/wwworker6.js
+++ src/wwworker6.js
@@ -1,7 +1,6 @@
 importScripts('http://davidbkemp.github.io/jsqubits/resources-0.0.1/js/jsqubits.js');
 
 self.addEventListener('message', function(e) {
-  self.postMessage(e.data);
-  self.postMessage('heee');
-  self.postMessage('' + jsqubits('101'));
+  self.postMessage('Evaluating');
+  self.postMessage('' + eval(e.data));
 }, false);
