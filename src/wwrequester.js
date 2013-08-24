6c251a57cc20dd2aec8a8e92d71712ba1f62824f
David Kemp <davidgithub@lizanddavid.com>
2013-08-24 19:19:07 +1000

    Update wwrequester.js

--- src/wwrequester.js
+++ src/wwrequester.js
@@ -2,6 +2,14 @@
 
 function handleClick() {
     alert($('#input').value);
+
+    var worker = new Worker('wwworker.js');
+
+    worker.addEventListener('message', function(e) {
+        alert('Worker said: ' + e.data);
+     }, false);
+
+     worker.postMessage($('#input').value);
 }
 
 $(function(){
