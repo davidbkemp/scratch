80d86629ac960e7b03730a838d6faeda91449550
David Kemp <davidbkemp@gmail.com>
2013-10-18 07:27:05 +0000

    start on animatedQubits

---
+++ src/animatedQubits/spec/animatedQubits.spec.js
@@ -0,0 +1,8 @@
+/*global describe:true, it:true, expect:true */
+
+describe("animatedQubits", function () {
+    "use strict";
+    it("should return an object with a display() method", function () {
+        expect("a").toBe("a");
+    });
+});
