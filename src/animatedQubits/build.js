790bbd4c34c4f6ce3aff35f65e6ad5bc52690a12
David Kemp <davidbkemp@gmail.com>
2013-11-11 23:16:48 +0000

    Update minification config to accomodate and exclude d3 and d3MeasureText

--- src/animatedQubits/build.js
+++ src/animatedQubits/build.js
@@ -1,8 +1,11 @@
 ({
     paths: {
       "animatedQubitsRenderer": "lib/animatedQubitsRenderer",
-      "qubitsGraphics": "lib/qubitsGraphics"
+      "qubitsGraphics": "lib/qubitsGraphics",
+      "d3": "empty:",
+      "d3MeasureText": "empty:"
     },
+    exclude: ['d3', 'd3MeasureText'],
     name: "animatedQubits",
     out: "animatedQubits.min.js",
     removeCombined: true
